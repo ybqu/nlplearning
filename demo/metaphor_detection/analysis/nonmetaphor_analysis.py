000e020b4e90853eeef474e990c0d65d575f1743
yuanbinqu <32428962+quxiansen@users.noreply.github.com>
2020-04-16 04:03:00 +0800

    :art: Rebuild demo

--- demo/metaphor_detection/analysis/nonmetaphor_analysis.py
+++ demo/metaphor_detection/analysis/nonmetaphor_analysis.py
@@ -11,35 +11,72 @@
 '''
 
 # here put the import lib
+
 import math
-import csv
 
 def main():
-
-    with open('./EnWords.csv', encoding='latin-1') as f:
-        lines = csv.reader(f)
-        next(lines)
-        words_list = {line[0]:i for i, line in enumerate(lines)}
-
-    target_words_list = []
-    with open('../mlm/non-metaphor/vua.out', 'r+') as f:
+    metaphor_indexs = []
+    ave_indexs = []
+    len_words = []
+    with open('../mlm/non-metaphor/trofix.out', 'r+', encoding='utf-8') as f:
         lines = f.readlines()
-        for line in lines:
-            raw_words_list = [words.split(',') for words in line.split()[2:]]
-            temp_words_list = []
-            for words in raw_words_list:
-                temp = [word for word in words if word in words_list]
-                temp_words_list.append(','.join(temp))
+        for i, line in enumerate(lines[2:]):
+            indexs = line.split(' ')[1].rstrip('\n')
+            indexs = [int(idx) for idx in indexs.split(',')]
 
-            temp = [line.split()[0], line.split()[1], ' '.join(temp_words_list)]
-            target_words_list.append(temp)
-
-    with open('./vua.out', 'w+') as f:
-        temp = [' '.join(words) for words in target_words_list]
-        f.write('\n'.join(temp))
-
-    print('保存完成！')
+            len_words.append(len(indexs) - 1)
+            
+            metaphor_indexs.append(int(indexs[0]))
+            ave_indexs.append(sum(indexs[1:]) // len_words[i])
 
 
+    metaphor_indexs.sort()
+    ave_indexs.sort()
+
+
+    print('max_预测词：{} min_预测词：{}'.format(max(len_words), min(len_words)))
+
+    meta_slice_nums = math.ceil(metaphor_indexs[len(metaphor_indexs) - 1] / 1000)
+    slice_nums = math.ceil(ave_indexs[len(ave_indexs) - 1] / 1000)
+    meta_slice_list = [i*1000 - 1 for i in range(meta_slice_nums + 1)]
+    slice_list = [i*1000 - 1 for i in range(slice_nums + 1)]
+
+    mpos_indexs = [0]
+    pos_indexs = []
+
+    for i, idx in enumerate(metaphor_indexs):
+        if idx > meta_slice_list[0]:
+            mpos_indexs.append(i)
+            del meta_slice_list[0]
+    
+    for i, idx in enumerate(ave_indexs):
+        if idx > slice_list[0]:
+            pos_indexs.append(i)
+            del slice_list[0]
+    
+    mpos_indexs.append(len(metaphor_indexs) - 1)
+    pos_indexs.append(len(ave_indexs) - 1)
+
+    mresult_len = [int(mpos_indexs[i+1] - mpos_indexs[i]) for i in range(len(mpos_indexs) - 1)]
+
+    result_len = [int(pos_indexs[i+1] - pos_indexs[i]) for i in range(len(pos_indexs) - 1)]
+
+    mresult_len[len(mresult_len)-1] += 1
+    result_len[len(result_len)-1] += 1
+
+    print('隐喻词：')
+    print('{:13}: {}'.format(-1, mresult_len[0]))
+    for i, length in enumerate(mresult_len):
+        if i == 0:
+            continue
+        print('{:5} ~ {:5}: {}'.format((i-1)*1000, i*1000 - 1, length))
+
+    print('非隐喻词：')
+    # print('{:13}: {}'.format(-1, result_len[0]))
+    for i, length in enumerate(result_len):
+        print('{:5} ~ {:5}: {}'.format((i)*1000, (i + 1)*1000 - 1, length))
+
+    print('非隐喻词共：{} 隐喻词共：{}'.format(sum(len_words), len(metaphor_indexs)))
+    
 if __name__ == "__main__":
     main()
